f5f98052adc86e01d0e0f097d1451864a2ffa7a5
Erkebulan <erkebulanermekbaj5@gmail.com>
2024-11-11 12:57:22 +0600

    экзамен4

---
+++ script.js
@@ -0,0 +1,43 @@
+var Ай = Number(prompt("1 - ден 12 - ге дейін сан енгіз"));
+
+switch (Ай) {
+    case 1:
+        alert(" Ай1, Қаңтар");
+        break;
+    case 2:
+        alert(" Ай2, Ақпан");
+        break;
+    case 3:
+        alert(" Ай3, Наурыз");
+        break;
+    case 4:
+        alert(" Ай4, Сәуір");
+        break;
+    case 5:
+        alert(" Ай5, Мамыр");
+        break;
+    case 6:
+        alert(" Ай6, Маусым");
+        break;
+    case 7:
+        alert(" Ай7, Шілде");
+        break;
+     case 8:
+        alert(" Ай8, Тамыз");
+        break;
+     case 9:
+        alert(" Ай9, Қыркүйек");
+        break;
+    case 10:
+        alert(" Ай10, Қазан");
+        break;
+    case 11:
+        alert(" Ай11, Қараша");
+        break;
+     case 12:
+        alert(" Ай12, Желтоқсан");
+        break;
+    default:
+        alert("1 - ден 12 - ге дейін сан енгіз");
+
+}
